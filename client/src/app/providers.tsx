2607f176238dd6932e163ebab772a6f33bc2e30f
grcndev <gleyce7mit@gmail.com>
2025-04-25 13:05:21 -0300

    removing front and back due to conflicting git history

--- client/src/app/providers.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-import { Provider } from "react-redux";
-import { store } from "../state/redux";
-
-export function ReduxProvider({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
-}
--- client/src/utils/format.js
+++
@@ -1,3 +0,0 @@
-export const capFirstLetter = (str) => {
-  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-};
